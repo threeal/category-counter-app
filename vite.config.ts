d1411f964f1d2de430b217cc0e123940e4e35857
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-13 16:14:27 +0700

    feat: update React app tooling
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+let base: string | undefined;
+if (process.env.GITHUB_REPOSITORY) {
+  base = process.env.GITHUB_REPOSITORY.slice(
+    process.env.GITHUB_REPOSITORY.indexOf("/"),
+  );
+}
+
 export default defineConfig({
-  base: "/category-counter-app",
+  base,
   plugins: [react()],
-})
+});
